b4c6234b6886917ededd13149e3939fa66751c51
Colin Davis <billfuc2@gmail.com>
2024-02-01 23:19:13 -0700

    I added set teamturn and validMoves

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -27,7 +27,7 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-        throw new RuntimeException("Not implemented");
+        this.turn = team;
     }
 
     /**
@@ -46,7 +46,8 @@ public class ChessGame {
      * startPosition
      */
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
-        throw new RuntimeException("Not implemented");
+        Collection<ChessMove> chessMoves = ChessPiece.pieceMoves(getBoard(), startPosition);
+        return chessMoves;
     }
 
     /**
