86675e2fb8dcf47619f1c9357888e686d63ffd81
Colin Davis <billfuc2@gmail.com>
2024-03-07 19:50:34 -0700

    I modified AccessGameData to use a database

--- server/src/main/java/dataAccess/AccessGameData.java
+++ server/src/main/java/dataAccess/AccessGameData.java
@@ -1,47 +1,98 @@
 package dataAccess;
 
 import chess.ChessGame;
+import com.google.gson.Gson;
+import model.AuthData;
 import model.GameData;
+import model.UserData;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.*;
 
 public class AccessGameData {
-    public Map<Integer , GameData> allGameData;
-    int curID;
+    private static Connection conn;
+    private static Gson gson = new Gson();
+    static {
+        try {
+            conn = DatabaseManager.getConnection();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public AccessGameData() {
-        this.allGameData = new HashMap<>();
-        this.curID = 1;
     }
-    public void clear() {
-        this.allGameData.clear();
+    static public void clear() throws Exception {
+        try (var preparedStatement = conn.prepareStatement("TURNCATE TABLE game")) {
+            preparedStatement.executeUpdate();
+        }
+        catch (SQLException e) {
+            throw new Exception(e.getMessage());
+        }
     }
-    public Integer createGame(String gameName) throws BadRequestException {
+    static public Integer createGame(String gameName) throws BadRequestException, DataAccessException {
         if (gameName != null) {
-            int gameID = curID;
-            curID++;
-            allGameData.put(gameID, new GameData(gameID, null, null, gameName, new ChessGame()));
-            return gameID;
+            ChessGame game = new ChessGame();
+            try (var preparedStatement = conn.prepareStatement("INSERT INTO game (gameName, game) VALUES(?, ?, ?)")) {
+                preparedStatement.setString(1, gameName);
+                preparedStatement.setString(2, gson.toJson(game));
+                preparedStatement.executeUpdate();
+            } catch (SQLException e) {
+                throw new DataAccessException(e.getMessage());
+            }
         }
         throw new BadRequestException("bad request");
+
     }
 
-    public Collection<GameData> listGames(){
-        return allGameData.values();
+    static public Collection<GameData> listGames() throws DataAccessException {
+        Collection<GameData> games = null;
+        try (var preparedStatement = conn.prepareStatement("SELECT * FROM game")) {
+            try (var rs = preparedStatement.executeQuery()) {
+                while (rs.next()) {
+                    games.add(new GameData(rs.getInt("gameID"), rs.getString("whiteUsername"), rs.getString("blackUsername"), rs.getString("gameName"), gson.fromJson(rs.getString("game"), ChessGame.class)));
+                }
+                return games;
+            }catch (SQLException e) {
+                throw new DataAccessException(e.getMessage());
+            }
+        }catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+    static public GameData getGame(int gameID) throws DataAccessException {
+        try (var preparedStatement = conn.prepareStatement("SELECT * FROM game WHERE gameID=?")) {
+            preparedStatement.setInt(1, gameID);
+            var rs = preparedStatement.executeQuery();
+            return new GameData(rs.getInt("gameID"), rs.getString("whiteUsername"), rs.getString("blackUsername"), rs.getString("gameName"), gson.fromJson(rs.getString("game"), ChessGame.class))
+        } catch (SQLException e) {
+            throw new DataAccessException("unauthorized");
+        }
     }
 
-    public void addBlackPlayer(String username, int gameID) throws AlreadyTakenException {
-        GameData gam = allGameData.get(gameID);
-        if(allGameData.get(gameID).blackUsername() != null){
+    static public void addBlackPlayer(String username, int gameID) throws AlreadyTakenException, DataAccessException {
+        if(getGame(gameID).blackUsername() != null){
             throw new AlreadyTakenException("already taken");
         }
-        allGameData.put(gameID, new GameData(gameID, gam.whiteUsername(), username, gam.gameName(), gam.game()));
+        try (var preparedStatement = conn.prepareStatement("UPDATE game SET blackUsername=? WHERE gameID=?")) {
+            preparedStatement.setString(1, username);
+            preparedStatement.setInt(2, gameID);
+            preparedStatement.executeUpdate();
+        }catch (SQLException e) {
+            throw new DataAccessException("unauthorized");
+        }
     }
-    public void addWhitePlayer(String username, int gameID) throws AlreadyTakenException, BadRequestException {
-        GameData gam = allGameData.get(gameID);
-        if(allGameData.get(gameID).whiteUsername() != null){
+    public void addWhitePlayer(String username, int gameID) throws AlreadyTakenException, BadRequestException, DataAccessException {
+        if(getGame(gameID).whiteUsername() != null){
             throw new AlreadyTakenException("already taken");
         }
-        allGameData.put(gameID, new GameData(gameID, username, gam.blackUsername(), gam.gameName(), gam.game()));
+        try (var preparedStatement = conn.prepareStatement("UPDATE game SET whiteUsername=? WHERE gameID=?")) {
+            preparedStatement.setString(1, username);
+            preparedStatement.setInt(2, gameID);
+            preparedStatement.executeUpdate();
+        }catch (SQLException e) {
+            throw new DataAccessException("unauthorized");
+        }
     }
 }
